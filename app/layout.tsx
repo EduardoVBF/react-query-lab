f8ded6b7ea9a18dc9f4dceb464ac266423925e94
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2026-05-11 22:24:19 -0300

    chore: update dependencies in package.json
    - Added new dependencies: class-variance-authority, clsx, radix-ui, shadcn, tailwind-merge, tw-animate-css
    - Updated existing dependencies to their latest versions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import { QueryProvider } from "@/providers/query-provider";
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,7 +22,7 @@ interface Props {
 
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={cn("font-sans", geist.variable)}>
       <body
         className={`${inter.className} bg-zinc-950 text-zinc-100 antialiased`}
       >
